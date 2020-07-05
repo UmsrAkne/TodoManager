2b0c27d69d35a0d7e3947049c26a9fe19c1352f7
Akn <sibata486@gmail.com>
2020-07-06 01:00:34 +0900

    機能追加 / データベースファイルを作成するメソッドを追加

--- TodoManager2/model/DatabaseHelper.cs
+++ TodoManager2/model/DatabaseHelper.cs
@@ -3,16 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SQLite;
 
 namespace TodoManager2.model {
     public class DatabaseHelper {
 
+        public String DatabaseName { get; private set; } = "";
+
         public DatabaseHelper(string databaseName) {
+            DatabaseName = databaseName;
             create();
         }
 
         private void create() {
-
+            SQLiteConnection.CreateFile(DatabaseName + ".sqlite");
         }
     }
 }
