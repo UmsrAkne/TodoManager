d8a2a1ca9531268a785e13ff1bdff2634db86653
Akn <sibata486@gmail.com>
2020-07-06 00:39:42 +0900

    クラス追加 / データベース接続補助のためのクラスと、そのテストクラスを追加

---
+++ TodoManager2/model/DatabaseHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoManager2.model {
+    public class DatabaseHelper {
+
+        public DatabaseHelper(string databaseName) {
+            create();
+        }
+
+        private void create() {
+
+        }
+    }
+}
---
+++ TodoManager2Tests/model/DatabaseHelperTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TodoManager2.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoManager2.model.Tests {
+    [TestClass()]
+    public class DatabaseHelperTests {
+
+        private string dbName = "testDatabase";
+
+        [TestMethod()]
+        public void DatabaseHelperTest() {
+            var dbh = new DatabaseHelper(dbName);
+        }
+    }
+}
