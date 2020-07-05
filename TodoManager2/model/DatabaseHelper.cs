cbd41626c752b848a53856576ce9ca794d4bc4b2
Akn <sibata486@gmail.com>
2020-07-06 01:27:14 +0900

    機能追加 / テーブルを新規作成するメソッド

--- TodoManager2/model/DatabaseHelper.cs
+++ TodoManager2/model/DatabaseHelper.cs
@@ -13,10 +13,30 @@ namespace TodoManager2.model {
         public DatabaseHelper(string databaseName) {
             DatabaseName = databaseName;
             create();
+            createTable("todos");
         }
 
         private void create() {
             SQLiteConnection.CreateFile(DatabaseName + ".sqlite");
+        }
+
+        private void createTable(String tableName) {
+            var createTableCommandText = "CREATE TABLE IF NOT EXISTS " + tableName + " ( " +
+                                         "name TEXT NOT NULL" +
+                                         ");";
+            executeNonQuery(createTableCommandText);
+        }
+
+        private void executeNonQuery(string commandText) {
+            using (var conn = new SQLiteConnection("Data Source=" + DatabaseName + ".sqlite")) {
+                conn.Open();
+                using(var command = conn.CreateCommand()) {
+                    command.CommandText = commandText;
+                    command.ExecuteNonQuery();
+                }
+
+                conn.Close();
+            }
         }
     }
 }
