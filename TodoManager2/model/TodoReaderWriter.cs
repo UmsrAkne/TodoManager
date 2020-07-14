745fef6d165f81d6c3eec789b044dfb0a4dad006
Akn <sibata486@gmail.com>
2020-07-14 20:08:33 +0900

    機能追加 / Todo オブジェクトをデータベースに追加する機能を追加

--- TodoManager2/model/TodoReaderWriter.cs
+++ TodoManager2/model/TodoReaderWriter.cs
@@ -9,9 +9,45 @@ namespace TodoManager2.model {
     public class TodoReaderWriter {
 
         private DatabaseHelper dbHelper;
+        private readonly string TABLE_NAME_TODOS = "todos";
 
         public TodoReaderWriter(DatabaseHelper dbHelper) {
             this.dbHelper = dbHelper;
+        }
+
+        public void add(Todo todo) {
+
+            long newID = 0;
+
+            if(dbHelper.getRecordCount(TABLE_NAME_TODOS) > 0) {
+                newID = dbHelper.getMaxInColumn(TABLE_NAME_TODOS, "id") + 1;
+            }
+
+            string[] columnNames = {
+                nameof(Todo.ID),
+                nameof(Todo.Title),
+                nameof(Todo.Text),
+                nameof(Todo.CreationDateTime),
+                nameof(Todo.CompletionDateTime),
+                nameof(Todo.DueDateTime),
+                nameof(Todo.Priority),
+                nameof(Todo.IsCompleted),
+                nameof(Todo.CompletionComment),
+            };
+
+            string[] values = {
+                newID.ToString(),
+                todo.Title,
+                todo.Title,
+                todo.CreationDateTime.ToString(),
+                todo.CompletionDateTime.ToString(),
+                todo.DueDateTime.ToString(),
+                todo.Priority,
+                todo.IsCompleted.ToString(),
+                todo.CompletionComment
+            };
+
+            dbHelper.insert(TABLE_NAME_TODOS, columnNames, values);
         }
     }
 }
