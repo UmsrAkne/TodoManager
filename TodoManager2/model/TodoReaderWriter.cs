e7bcc0e70829ba1cf709be87aec9cea5d9511482
Akn <sibata486@gmail.com>
2020-07-14 18:36:04 +0900

    クラス追加 / Todo の読み込み、書き込みを行うクラスファイルを作成

---
+++ TodoManager2/model/TodoReaderWriter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// このクラスでは Todo オブジェクトをデータベースに書き込んだり、List として取り出したりする機能を提供します。
+/// </summary>
+
+namespace TodoManager2.model {
+
+    public class TodoReaderWriter {
+
+        private DatabaseHelper dbHelper;
+
+        public TodoReaderWriter(DatabaseHelper dbHelper) {
+            this.dbHelper = dbHelper;
+        }
+    }
+}
