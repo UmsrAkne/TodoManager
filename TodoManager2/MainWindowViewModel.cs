e1dfd4499e08886ef33098b5b9d644e0b0fb92a3
Akn <sibata486@gmail.com>
2020-07-19 16:38:55 +0900

    機能追加 / Todo追加ボタンに対するコマンドを追加

--- TodoManager2/MainWindowViewModel.cs
+++ TodoManager2/MainWindowViewModel.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Prism.Mvvm;
+using Prism.Commands;
 using TodoManager2.model;
 
 namespace TodoManager2 {
@@ -11,6 +12,23 @@ namespace TodoManager2 {
 
         public Todo CreatingTodo {
             get; set;
+        } = new Todo();
+
+        private DelegateCommand addTodoCommand;
+        public DelegateCommandBase AddTodoCommand {
+            get {
+                return addTodoCommand ?? (addTodoCommand = new DelegateCommand(
+                    () => {
+                        Todo todo = new Todo(DateTime.Now, 0);
+                        todo.Title = CreatingTodo.Title;
+                        todo.Text = CreatingTodo.Text;
+                        todo.WorkSpan = CreatingTodo.WorkSpan;
+                        todo.Priority = CreatingTodo.Priority;
+                        todo.IsCompleted = CreatingTodo.IsCompleted;
+                        todo.DueDayNumber = CreatingTodo.DueDayNumber;
+                    }
+                ));
+            }
         }
     }
 }
