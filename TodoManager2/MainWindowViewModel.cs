a990b0b9d50c3abba7d74a1db94bb71b13339494
Akn <sibata486@gmail.com>
2020-07-19 16:02:30 +0900

    MainWindow の一部項目と、MainWindowViewModel のプロパティをバインド

--- TodoManager2/MainWindowViewModel.cs
+++ TodoManager2/MainWindowViewModel.cs
@@ -4,8 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Prism.Mvvm;
+using TodoManager2.model;
 
 namespace TodoManager2 {
     class MainWindowViewModel : BindableBase{
+
+        public Todo CreatingTodo {
+            get; set;
+        }
     }
 }
